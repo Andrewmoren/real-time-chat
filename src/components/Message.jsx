006c176e46fcba107061ec961e2951dcc10ac321
Andrew <andrewmoren93@gmail.com>
2022-11-24 09:49:51 +0300

    add router

---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <div>message</div>;
+};
+
+export default Message;
---
+++ src/components/routes.js
@@ -0,0 +1,15 @@
+import { LOGIN_ROUTE } from "../utils/conts";
+
+export const publicRoutes = [
+  {
+    path: LOGIN_ROUTE,
+    Component: Login,
+  },
+];
+
+export const privateRoutes = [
+  {
+    path: CHAT_ROUTE,
+    Component: Chat,
+  },
+];
